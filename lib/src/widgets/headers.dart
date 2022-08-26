8cb99296e92657e62b3785c8483446ff74fc07b0
Jeancarlos  Carvajal <jeancarloscarvajalavila@gmail.com>
2022-08-26 18:23:36 -0400

    Iniciando el Header

---
+++ lib/src/widgets/headers.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+ class HeadersCuadro extends StatelessWidget {
+   const HeadersCuadro({Key? key}) : super(key: key);
+ 
+   @override
+   Widget build(BuildContext context) {
+     return  Container(
+      height: 300,
+      color: Color(0xff615aab),
+    );
+   }
+ }
